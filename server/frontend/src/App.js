5f6bc5806af2423dd146c5eaa389999c59ca5475
Aco Vidovic <acovid@gmail.com>
2025-04-08 17:04:18 +0200

    Add a 'Register' functionality

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,13 @@
 import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import RegisterPanel from "./components/Register/Register"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel />} />
     </Routes>
-  );
+  )
 }
-export default App;
+export default App
